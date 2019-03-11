7d2adf16f7353887dc390eb40547d98733b28582
przand1 <przand1@st.amu.edu.pl>
2019-03-11 17:26:27 +0100

    complex test update

--- Main.java
+++ Main.java
@@ -9,21 +9,23 @@ public class Main {
     Complex B = new Complex(3,-4);
     Complex C = Complex.add(A,B);
     Complex D = Complex.sub(A,B);
+    System.out.println(A+" + "+B+" = ");
     System.out.println(C);
+    System.out.println(A+" - "+B+" = ");
     System.out.println(D);
     System.out.println();
     A.setReal(10);A.setImg(-7);
     B.setReal(-13);B.setImg(4);
-    C = Complex.add(A,B);
-    D = Complex.sub(A,B);
+    System.out.println(A+" + "+B+" = ");
     System.out.println(C);
+    System.out.println(A+" - "+B+" = ");
     System.out.println(D);
     System.out.println();
     A.setReal(-1);A.setImg(-6);
     B.setReal(-1);B.setImg(7);
-    C = Complex.add(A,B);
-    D = Complex.sub(A,B);
+    System.out.println(A+" + "+B+" = ");
     System.out.println(C);
+    System.out.println(A+" - "+B+" = ");
     System.out.println(D);
     System.out.println();
 
@@ -32,14 +34,18 @@ public class Main {
     B.setReal(3);B.setImg(0);
     C = Complex.mul(A,B);
     D = Complex.div(A,B);
+    System.out.println(A+" * "+B+" = ");
     System.out.println(C);
+    System.out.println(A+" / "+B+" = ");
     System.out.println(D);
     System.out.println();
     A.setReal(1);A.setImg(-8);
     B.setReal(-3);B.setImg(-1);
     C = Complex.mul(A,B);
     D = Complex.div(A,B);
+    System.out.println(A+" * "+B+" = ");
     System.out.println(C);
+    System.out.println(A+" / "+B+" = ");
     System.out.println(D);
     System.out.println();
 
@@ -47,9 +53,14 @@ public class Main {
     Complex c2 = new Complex(2,new Argument(5,6));
     System.out.println(c1+" * "+c2+" = ");
     System.out.println(Complex.mul(c1,c2));
+    System.out.println();
 
     Complex c3 = c1.pow(4);
     System.out.println(c1+"^4 = "+c3);
+    System.out.println();
+
+    System.out.println("sprzężenie "+A);
+    System.out.println(A.conjugate());
 
   }
 }
